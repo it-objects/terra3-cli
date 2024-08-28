970559288242c1cee639e43c166d491a4c55e584
Widura Schwittek <widura.schwittek@it-objects.de>
2024-08-28 09:30:17 +0200

    feat(semver): introduce auto versioning

--- cmd/version.go
+++ cmd/version.go
@@ -9,13 +9,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// var needs to be used instead of const as ldflags is used to fill this
+// information in the release process
+var (
+	version = "unknown"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints out Terra3 CLI version.",
 	Long:  `Prints out Terra3 CLI version.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Terra3 CLI v0.0.7")
+		fmt.Println("Terra3 CLI " + version + " Date: " + date + " Commit: " + commit)
 	},
 }
 
